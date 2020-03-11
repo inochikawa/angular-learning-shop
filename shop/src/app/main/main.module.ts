55a56c55c483549b18998e27c7dbbecaaef07cc6
Maxim Stecenko <maxim.stecenko@limestonedigital.co>
2020-03-11 02:24:41 +0200

    Fix modules files

--- shop/src/app/main/main.module.ts
+++ shop/src/app/main/main.module.ts
@@ -1,13 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from './app-routing.module';
 import { ShopRootComponent } from './shop-root/shop-root.component';
 
-
-
 @NgModule({
-  declarations: [ShopRootComponent],
+  declarations: [
+    ShopRootComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [ShopRootComponent]
 })
 export class MainModule { }
